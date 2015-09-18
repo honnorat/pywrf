4b8a04928f2ee241b001a87d879cc4d6fd1d02ad
Marc Honnorat <marc.honnorat@gmail.com>
2015-09-18 02:27:35 +0200

    Update license notice

--- pywrf/__init__.py
+++ pywrf/__init__.py
@@ -1,1 +1,8 @@
+# -*- coding: utf-8 -*-
+# Licensed under the Apache License, Version 2.0 - see LICENSE file;
+
+__title__ = 'pywrf'
 __version__ = '0.1dev'
+__author__ = 'Marc Honnorat'
+__license__ = 'Apache 2.0'
+__copyright__ = 'Copyright 2015 Marc Honnorat'
